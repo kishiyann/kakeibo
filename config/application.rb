8faa78bd8eef0e1b41ba7f804940506f9909e603
kishiyann <to5hi5_0315@yahoo.co.jp>
2020-05-08 17:19:39 +0900

    収入科目

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module Kakeibo
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    # ↓paramsの保存指定を全て通るようにしている
+    config.action_controller.permit_all_parameters = true
   end
 end
