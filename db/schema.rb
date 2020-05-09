4632d7cf76f4faf164c0a719fd7f4c01ebf960b6
kishiyann <to5hi5_0315@yahoo.co.jp>
2020-05-09 10:15:42 +0900

    固定費

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_08_072958) do
+ActiveRecord::Schema.define(version: 2020_05_09_005001) do
+
+  create_table "fixedcosts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "incomes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
